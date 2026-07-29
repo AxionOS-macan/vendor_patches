1fffd013a977ebc8f3a428486e66199a52baf4d8
blazerpaul15 <blazerpaul15@gmail.com>
2026-07-29 09:15:43 +0900

    patches: skip already applied patches

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,10 +10,18 @@ apply_patches() {
         cd ${TOP}
         cd ${project_path}
         echo "Applying patches for project: ${project_name} on ${HEAD_COMMIT}"
-        if ! git am "${PATCHES_PATH}"/${project_name}/*.patch --no-gpg-sign; then
-            echo "Failed to apply patches for project: ${project_name}. Aborting."
-            git am --abort &> /dev/null
-        fi
+        for patch in "${PATCHES_PATH}"/${project_name}/*.patch; do
+            if git apply --reverse --check "${patch}" &> /dev/null; then
+                echo "Skipping already applied patch: $(basename "${patch}")"
+                continue
+            fi
+
+            if ! git am "${patch}" --no-gpg-sign; then
+                echo "Failed to apply patch: ${patch}. Aborting."
+                git am --abort &> /dev/null
+                break
+            fi
+        done
         cd ${TOP}
     done
 }
